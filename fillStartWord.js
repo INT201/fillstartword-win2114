4f5d937b07cba48819c35f32ec65a88cfdf47da5
win2114 <winwin14.lt@gmail.com>
2022-09-11 15:40:10 +0700

    fillstart

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,12 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  if(word === null || word === undefined){
+    return undefined
+  }
+  if(word.startsWith(startWord)){
+    return word
+  }
+  return startWord.concat(word)
 }
 module.exports = fillStartWord
